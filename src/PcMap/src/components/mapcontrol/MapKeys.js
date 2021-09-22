c2c1902b2b56428da400517ed1034d9c7d48ee9a
pcwang <wang_wang_lao@163.com>
2021-09-22 19:29:12 +0800

    增加qq地图

--- src/PcMap/src/components/mapcontrol/MapKeys.js
+++ src/PcMap/src/components/mapcontrol/MapKeys.js
@@ -3,7 +3,8 @@ const MapKeys = {
   tianditu: '请填写从天地图网站申请到的地图服务key',
   gaode: '请填写从高德地图网站申请到的地图服务key',
   bing: '请填写从必应地图网站申请到的地图服务key',
-  baidu: '请填写从百度地图网站申请到的地图服务key'
+  baidu: '请填写从百度地图网站申请到的地图服务key',
+  qq: '请填写从腾讯地图网站申请到的地图服务key'
 }
 
 export {
