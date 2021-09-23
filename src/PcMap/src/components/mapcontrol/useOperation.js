d3414b5dfe0bbe0b0b7ef0ce62e68d9853b0c401
pcwang <wang_wang_lao@163.com>
2021-09-23 19:07:55 +0800

    增加Map控件的绘制操作

---
+++ src/PcMap/src/components/mapcontrol/useOperation.js
@@ -0,0 +1,16 @@
+const OperationType = {
+  NOTHING: 0,
+  CLICK: 1,
+  DRAW_POINT: 2,
+  DRAW_LINESTRING: 3,
+  DRAW_POLYGON: 4
+}
+
+function userOperation (control) {
+
+}
+
+export {
+  OperationType,
+  userOperation
+}
