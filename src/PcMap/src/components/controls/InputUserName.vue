43b6cd32f264ca8c7d2c23f2a09eecec98cfe708
pcwang （麓山老将） <wang_wang_lao@163.com>
2021-10-11 13:32:43 +0800

    增加用户名有效性验证，必须为email或移动电话

--- src/PcMap/src/components/controls/InputUserName.vue
+++ src/PcMap/src/components/controls/InputUserName.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-input dense outlined abel="Outlined" label="用户名(EMail或移动电话)">
+  <q-input v-model="inputValue" @update:model-value="onInputChanged" dense outlined label="用户名(EMail或移动电话)" @blur="onBlur">
     <template v-slot:prepend>
       <q-icon name="o_person" />
     </template>
@@ -8,9 +8,34 @@
 
 <script>
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { useQuasar } from 'quasar'
+import { isEmail, isMobile } from './Validator.js'
 
 export default defineComponent({
+  setup (props, context) {
+    const inputValue = ref('')
+    const $q = useQuasar()
+
+    const onBlur = () => {
+      if (!isEmail(inputValue.value) && !isMobile(inputValue.value)) {
+        $q.notify({
+          message: '请输入有效的email或者移动电话号码',
+          position: 'top',
+          timeout: 1500
+        })
+      }
+    }
+
+    const onInputChanged = (val) => {
+      inputValue.value = val
+    }
+    return {
+      inputValue,
+      onBlur,
+      onInputChanged
+    }
+  }
 })
 
 </script>
